35f9d88e73433932d604df9ca54e462faa4d2a17
Matt Riggott <matt.riggott@gmail.com>
2009-06-19 21:09:20 +0100

    Added common settings and URLs.

---
+++ flother/settings/common.py
@@ -0,0 +1,47 @@
+import os
+import sys
+
+
+SITE_ROOT = os.path.join(os.path.dirname(os.path.realpath(__file__)), '..')
+
+MEDIA_ROOT = os.path.join(SITE_ROOT, 'media')
+MEDIA_URL = '/media/'
+ADMIN_MEDIA_PREFIX = MEDIA_URL + 'admin/'
+
+LANGUAGE_CODE = 'en-gb'
+TIME_ZONE = 'Europe/London'
+USE_I18N = False
+DATE_FORMAT = 'j F Y'
+TIME_FORMAT = 'P'
+DATETIME_FORMAT = ', '.join([TIME_FORMAT, DATE_FORMAT])
+MONTH_DAY_FORMAT = 'j F'
+
+ROOT_URLCONF = 'flother.urls'
+
+TEMPLATE_CONTEXT_PROCESSORS = (
+    'django.core.context_processors.auth',
+    'django.core.context_processors.media',
+)
+TEMPLATE_DIRS = (
+    os.path.join(SITE_ROOT, 'templates'),
+)
+TEMPLATE_LOADERS = (
+    'django.template.loaders.filesystem.load_template_source',
+    'django.template.loaders.app_directories.load_template_source',
+)
+ 
+MIDDLEWARE_CLASSES = (
+    'django.middleware.http.ConditionalGetMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.middleware.gzip.GZipMiddleware',
+)
+
+INSTALLED_APPS = (
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.sites',
+    'django.contrib.admin',
+)
