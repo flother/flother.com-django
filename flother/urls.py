6f80b0c162257f9ee7e47e7ecb0aa39d655d3d88
Matt Riggott <matt.riggott@gmail.com>
2009-06-19 21:44:20 +0100

    Duplicate of the current home page, minus the contact form, now in.

--- flother/urls.py
+++ flother/urls.py
@@ -1,17 +1,19 @@
-from django.conf.urls.defaults import *
+from django.conf import settings
+from django.conf.urls.defaults import include, patterns, handler404, handler500
+from django.contrib import admin
+from django.views.generic.simple import direct_to_template
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+
+admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Example:
-    # (r'^flother2/', include('flother2.foo.urls')),
-
-    # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
-    # to INSTALLED_APPS to enable admin documentation:
-    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # (r'^admin/', include(admin.site.urls)),
+    (r'^$', direct_to_template, {'template': 'home.html'}),
+    (r'^admin/', include(admin.site.urls)),
 )
+
+if settings.DEBUG:
+    from django.views.static import serve
+    urlpatterns += patterns('',
+        (r'^media/(?P<path>.*)$', serve, {'document_root':
+            settings.MEDIA_ROOT}),
+    )
