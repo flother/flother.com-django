9ed1f2fbd9e5ca5edc3759d5986194724ad3218f
Matt Riggott <matt.riggott@gmail.com>
2009-06-27 18:13:55 +0100

    Entry.number_of_views incremented each time the entry is viewed.

--- flother/apps/blog/views.py
+++ flother/apps/blog/views.py
@@ -27,6 +27,8 @@ def entry_detail(request, year, slug):
     """
     entry = get_object_or_404(Entry.objects.published(), published_at__year=year,
         slug=slug)
+    entry.number_of_views += 1
+    entry.save()
     context = {'entry': entry}
     return render_to_response('blog/entry_detail.html', context,
         RequestContext(request))
