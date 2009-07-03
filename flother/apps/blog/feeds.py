0ff119ff8efbcdbf8b3e36e1c08a0d676f50aad9
Matt Riggott <matt.riggott@gmail.com>
2009-07-03 22:51:28 +0100

    Added an Atom feed for the blog.

---
+++ flother/apps/blog/feeds.py
@@ -0,0 +1,31 @@
+from django.contrib.sites.models import Site
+from django.contrib.syndication.feeds import Feed
+from django.core.urlresolvers import reverse
+from django.utils.feedgenerator import Atom1Feed
+
+from flother.apps.blog.models import Entry
+
+
+site = Site.objects.get_current()
+
+
+class LatestEntries(Feed):
+    """An Atom 1.0 feed of the latest ten public entries from the blog."""
+
+    feed_type = Atom1Feed
+    title = u'%s: latest entries' % site.name
+    subtitle = 'More than a hapax legomenon.'
+    description_template = 'blog/feed_latest_description.html'
+
+    def link(self):
+        from flother.apps.blog.views import entry_index
+        return reverse(entry_index)
+
+    def items(self):
+        return Entry.objects.published().select_related()[:10]
+
+    def item_author_name(self, item):
+        return item.author.first_name or item.author.username
+
+    def item_pubdate(self, item):
+        return item.published_at
--- flother/apps/blog/urls.py
+++ flother/apps/blog/urls.py
@@ -1,6 +1,13 @@
 from django.conf.urls.defaults import patterns, url
+from django.contrib.syndication.views import feed
 
 from flother.apps.blog import views
+from flother.apps.blog.feeds import LatestEntries
+
+
+feeds = {
+    'latest': LatestEntries,
+}
 
 
 urlpatterns = patterns('',
@@ -9,4 +16,5 @@ urlpatterns = patterns('',
         name='blog_entry_archive_year'),
     url(r'^(?P<year>\d{4})/(?P<slug>[a-z0-9\-]+)/$', views.entry_detail,
         name='blog_entry_detail'),
+    url(r'^feeds/(.*)/$', feed, {'feed_dict': feeds}, name='blog_feeds'),
 )
