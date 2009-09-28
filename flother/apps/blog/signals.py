8c3d97f02a58f0e2df243465e3edd826d7ed903a
Matt Riggott <matt.riggott@gmail.com>
2009-09-28 13:19:56 +0100

    Updated the blog post-save/delete signal so the next and previous entry caches are deleted too (those pages contain links to the blog entry that's changed, so the cache could well be out-of-date).

--- flother/apps/blog/signals.py
+++ flother/apps/blog/signals.py
@@ -1,15 +1,35 @@
+from django.core.exceptions import ObjectDoesNotExist
 from django.core.urlresolvers import reverse
 from staticgenerator import quick_delete
 
 
 def delete_blog_index(sender, instance, **kwargs):
-    stagnant_cache_urls = (
+    """
+    Delete all files in the StaticGenerator cache that will be
+    out-of-date after a blog entry is saved.  These are:
+
+      * Blog index
+      * Archive page for the year the entry was published
+      * Admin users' preview page
+      * Blog Atom 1.0 feed
+      * Page for the blog entry itself
+      * Previously published entry's page
+      * Next published entry's page
+    """
+    stagnant_cache_urls = [
         reverse('blog_entry_index'),
         reverse('blog_entry_archive_year', args=[instance.published_at.year]),
-        reverse('blog_entry_detail', args=[instance.published_at.year,
-            instance.slug]),
         reverse('blog_entry_preview', args=[instance.published_at.year,
             instance.slug]),
         reverse('blog_feeds', args=['latest']),
-    )
+        instance.get_absolute_url(),
+    ]
+    try:
+        stagnant_cache_urls.append(instance.get_next_published_entry())
+    except ObjectDoesNotExist:
+        pass
+    try:
+        stagnant_cache_urls.append(instance.get_previous_published_entry())
+    except ObjectDoesNotExist:
+        pass
     quick_delete(*stagnant_cache_urls)
