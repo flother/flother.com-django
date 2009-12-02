9d610e1e14e13232d71f430069347bf3468e08eb
Matt Riggott <matt.riggott@gmail.com>
2009-12-02 20:45:43 +0000

    Changed the CommentFormForCaching class so it properly bypasses Django's security form fields.  The caching I have on the site means I can't use the time-based security Django uses by default.

--- flother/apps/comments/forms.py
+++ flother/apps/comments/forms.py
@@ -6,14 +6,22 @@ from django.forms.util import ErrorDict
 
 
 class CommentFormForCaching(CommentForm):
+
+    """
+    Django's CommentForm class minus the security fields.
+
+    Because the site is heavily cached the security fields won't work as
+    they're based on timestamps that will go out-of-date very quickly on a
+    cached page.
+    """
+
     def security_errors(self):
+        """Return just those errors associated with security."""
         errors = ErrorDict()
         if 'honeypot' in self.errors:
             errors['honeypot'] = self.errors['honeypot']
         return errors
 
-    def initial_security_hash(self, timestamp):
-        return ''
-
     def generate_security_hash(self, content_type, object_pk, timestamp):
-        return ''
+        """Generate a SHA1 security hash."""
+        return 'ce7501007f04a6529e650f1f1b3fc0586d1d94eb'
