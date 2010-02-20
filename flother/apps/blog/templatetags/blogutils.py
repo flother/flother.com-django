9a6a2f37b5a6ca70396fd5783309369d6f48a9f2
Matt Riggott <matt.riggott@gmail.com>
2010-02-20 17:49:44 +0000

    Strip pullquotes from blog entries in the Atom feed.
    The pullquotes are confusing and out of place when there's no CSS applied (i.e. when they're in the Atom feed) so they're now removed by a template tag named strip_pullquotes.

--- flother/apps/blog/templatetags/blogutils.py
+++ flother/apps/blog/templatetags/blogutils.py
@@ -1,3 +1,5 @@
+import re
+
 from django import template
 from django.conf import settings
 from django.utils.html import escape
@@ -9,6 +11,8 @@ register = template.Library()
 
 DEFAULT_GRAVATAR_IMAGE = '%score/img/avatar.png' % settings.MEDIA_URL
 GRAVATAR_RATING = 'r'
+PULLQUOTE_RE = re.compile(r'<blockquote\sclass="pullquote">.+?</blockquote>',
+    re.UNICODE)
 
 
 @register.simple_tag
@@ -18,3 +22,22 @@ def gravatarimg(email, size=32):
         size, GRAVATAR_RATING, DEFAULT_GRAVATAR_IMAGE)
     return '<img alt="Gravatar" height="%s" src="%s" width="%s" />' % (size,
         escape(url), size)
+
+
+@register.filter
+def strip_pullquotes(copy):
+    """
+    Strip pullquotes from the given blog entry copy.  This is used
+    in the Atom feed template, as the pullquotes are confusing and out
+    of place without CSS applied.
+
+    As an example, given the string::
+
+    >>> s = '<p>Lorem <a href="#">ipsum</a>.</p><blockquote class="pullquote"><p>Dolor sit amet</p></blockquote><p>consectetur adipisicing elit</p>'
+    >>> strip_pullquotes(s)
+    '<p>Lorem <a href="#">ipsum</a>.</p><p>consectetur adipisicing elit</p>'
+    >>> s = '<blockquote><p>Lorem ipsum</p></blockquote><blockquote class="pullquote"><p>Dolor sit amet</p></blockquote><blockquote><p>consectetur adipisicing elit</p></blockquote>'
+    >>> strip_pullquotes(s)
+    '<blockquote><p>Lorem ipsum</p></blockquote><blockquote><p>consectetur adipisicing elit</p></blockquote>'
+    """
+    return PULLQUOTE_RE.sub('', copy)
